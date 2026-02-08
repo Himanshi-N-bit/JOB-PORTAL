9162058ad94edc279f8d675d7540a8a37b95d334
Himanshi <himanshichauhan738@gmail.com>
2026-02-08 21:10:08 +0530

    Initial Job Portal MERN project

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
+const app = express();
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: ["http://localhost:5121"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+const PORT = process.env.PORT || 5001;
+app.listen(PORT,()=>{
+     console.log(`Server is running on ${PORT}`);
+});    
